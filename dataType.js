4dfbdbaffee5ee5ac01b0450c8ab2c0b15d5d1c1
Samitha Athurupana <athurupana1998@gmail.com>
2026-08-16 04:11:28 +0530

    data type - string

---
+++ dataType.js
@@ -0,0 +1,19 @@
+/*
+Primitive
+    String, Number, Boolean, Undefine, Null, BigInt, Symbol
+
+Non-Primitive
+    Object
+ */
+
+let name = "Malith";
+console.log(name);
+
+let myName = 'Dasun';
+console.log(myName);
+
+
+// literals
+let liName = "John";
+
+console.log('hello ${liName}!');
