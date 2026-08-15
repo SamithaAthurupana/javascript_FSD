569842088e310d2f9bf9ade1110b7a55f4cf24d1
Samitha Athurupana <athurupana1998@gmail.com>
2026-08-16 03:08:22 +0530

    initialize js variables

---
+++ variable.js
@@ -0,0 +1,39 @@
+/*
+let
+var
+const
+ */
+
+let age = 25;
+
+console.log(age);
+age = 5;
+console.log(age);
+
+// const name = "John";
+// console.log(name);
+// name = "n";
+// console.log(name);
+
+var name = "John";
+console.log(name);
+name = "J";
+console.log(name);
+
+
+/*
+Naming Convention
+camel case
+    let firstName;
+
+    let userAge;
+
+    *let 123Name; - wrong
+    *we can not use reserved words also - (abstract, boolean, catch, continue...etc)
+ */
+
+let firstName = name;
+console.log(firstName);
+
+// let  = name;
+console.log(firstName);
