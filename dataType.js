69808108f51a9871465d1babc9d8e591ce1d451f
Samitha Athurupana <athurupana1998@gmail.com>
2026-08-24 03:41:50 +0530

    Variable

--- dataType.js
+++ dataType.js
@@ -6,14 +6,56 @@ Non-Primitive
     Object
  */
 
-let name = "Malith";
-console.log(name);
+// String variable
+// let name = "Sadun";
+// console.log(name);
 
-let myName = 'Dasun';
-console.log(myName);
-
+// let name = 'Malith';
+// console.log(name);
 
 // literals
-let liName = "John";
+// let name = "Isuru";
+// console.log('Hello '+name+'!..'+' How much do you want '+name+'?');
+// console.log(`Hello, ${name}`);
 
-console.log('hello ${liName}!');
+//Numbers
+let age = 25;
+let price = 1500.50;
+console.log(age, price);
+
+let a = 10;
+let b = 5;
+
+//operations
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+
+//Boolean
+// true, false
+let isLoggedIn = true;
+let isAdmin = false;
+
+console.log(isAdmin);
+
+//Undefine
+let num1;
+console.log(num1);
+
+let num2 = null;
+console.log(num2);
+
+//type of
+let num3 = "John";
+console.log(num3);
+console.log(typeof num3);
+console.log(typeof true);
+console.log(typeof 100);
+
+
+
+
+
+
+
