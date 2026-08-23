dfafd3ce86ed6cb7f893dcba1b9f686ecd87ad60
Samitha Athurupana <athurupana1998@gmail.com>
2026-08-24 04:15:26 +0530

    if statement

---
+++ if_Statement.js
@@ -0,0 +1,25 @@
+// If statement
+// let money = 200;
+// if (money >= 1800){
+//     console.log("Start Payment.. Payment Done!");
+// }else {
+//     console.log("Not enough money to payment");
+// }
+
+
+// else if
+let marks  = 5;
+if (marks >= 75) {
+    console.log("A");
+}else if (marks >= 65) {
+    console.log("B");
+}else if (marks >= 55) {
+    console.log("C");
+}else {
+    console.log("Fail");
+}
+
+// Ternary Operator
+let age = 50;
+let result = age>= 18 ? "adult":"child";
+console.log(result);
